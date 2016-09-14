79e52157ff3d49bb3ce5d57d0fdd212567f19d10
Rob Sturcke <rs@rsvision.com>
2016-09-14 16:58:25 -0400

    SCSS Posts and User control and model made

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,56 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+    @post = Post.new
+  end
+
+  def show
+  end
+
+  def new
+    @post = current_user.posts.last
+  end
+
+  def edit
+
+  end
+
+  def create
+    @post.user = current_user
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:notice] = "Posted!"
+      redirect_to @post
+    else
+      flash[:error] = "Not posted, try again!"
+      render :new
+    end
+  end
+
+  def update
+    @post.assign_attributes(post_params)
+    if @post.save
+      flash[:notice] = "Updated!"
+      redirect_to @post
+    else
+      flash[:error] = "Not updated, try again!"
+      render :new
+    end
+  end
+
+  def destroy
+    if @post.destroy
+      flash[:notice] = "Deleted your posting!"
+      redirect_to posts_path
+    else
+      flash[:error] = "Posting not killed, try again!"
+      redirect_to :back
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:name)
+  end
+end
---
+++ app/models/post.rb
@@ -0,0 +1,9 @@
+class Post < ActiveRecord::Base
+  belongs_to :user
+
+  def post_time_out
+    create_date = created_at.to_date
+    (create_date + 45.minutes - create_date).to_i
+  end
+  
+end
