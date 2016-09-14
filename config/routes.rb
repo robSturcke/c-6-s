f534ce30fe9e78acb4571146e71cd5a97bda1e26
Rob Sturcke <rs@rsvision.com>
2016-09-14 17:46:18 -0400

    Pages added also DUMMY CSS for structure reasons

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-
   devise_for :users
-  root to: "home#index"
+  resources :posts
+
+  root "posts#index"
+
+  get '/about', to: 'pages#about'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
