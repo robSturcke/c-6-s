268559fc79236ba548f28aab7046ef161fb5e133
Rob Sturcke <rs@rsvision.com>
2016-09-26 18:58:13 -0400

    stripe integration

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   root "posts#index"
 
   get '/about', to: 'pages#about'
+
+  resources :charges, only: [:new, :create]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
