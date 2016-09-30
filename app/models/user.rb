73a048cfe4b343e95aa95ce62fcd66506d827774
Rob Sturcke <rs@rsvision.com>
2016-09-29 21:37:22 -0400

    invite code added

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,19 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validate :validate_invite, :on => :create
+  attr_accessor :invite
+
   has_many :posts
 
   belongs_to :role
   before_create :set_default_role
+
+  def validate_invite
+    if self.invite != "technodrome"
+      self.errors[:base] << "The invite code you've entered is incorrect."
+    end
+  end
 
   private
   def set_default_role
