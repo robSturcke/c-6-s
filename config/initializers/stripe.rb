8d24b1a8d6aa2cfd7175d7d3e4a035e10369db38
Rob Sturcke <rs@rsvision.com>
2016-12-15 01:51:07 -0500

    fix for recaptcha production not working

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -3,6 +3,5 @@
    publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
    secret_key: ENV['STRIPE_SECRET_KEY']
  }
-
  # Set our app-stored secret key with Stripe
  Stripe.api_key = Rails.configuration.stripe[:secret_key]
